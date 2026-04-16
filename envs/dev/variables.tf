cc50a0da19d6c916f465e7f31b33440c02d91e38
Ismail Cawni <ismail@example.com>
2026-04-16 14:39:51 +0200

    Role1 done Platform

--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -1,37 +1,32 @@
 variable "aws_region" {
-  description = "Region AWS cible. eu-west-1 (Irlande) imposee par le CdC RGPD."
+  description = "Region AWS (impose : eu-west-1 pour RGPD)."
   type        = string
-  default     = "eu-west-1"
+  default     = "eu-west-3"
 
   validation {
     condition     = var.aws_region == "eu-west-1"
-    error_message = "La region doit etre eu-west-1 (exigence T1 du CdC)."
+    error_message = "La region doit etre eu-west-3 (exigence T1 du CdC)."
   }
 }
 
 variable "allowed_admin_cidr" {
-  description = "CIDR IP source autorise pour l acces HTTPS a l ALB (IP formateur)."
+  description = "CIDR IP autorise a atteindre l ALB en HTTPS (IP formateur)."
   type        = string
 
   validation {
     condition     = can(cidrhost(var.allowed_admin_cidr, 0))
     error_message = "Doit etre un CIDR IPv4 valide (ex: 203.0.113.42/32)."
   }
-
-  validation {
-    condition     = var.allowed_admin_cidr != "0.0.0.0/0"
-    error_message = "Ouvrir au monde entier (0.0.0.0/0) est interdit en TP."
-  }
 }
 
 variable "project_name" {
-  description = "Nom de projet utilise pour tagger les ressources."
+  description = "Nom de projet pour le tagging."
   type        = string
   default     = "kolab"
 }
 
 variable "environment" {
-  description = "Nom de l environnement (dev, staging, prod)."
+  description = "Nom de l environnement."
   type        = string
   default     = "dev"
 }
